12023025c7615acd3fc5eeeac0824607ced12ff1
Shangkun Shen <polossk@126.com>
2021-01-16 18:07:06 +0800

    Update Example_C.ps1

--- Group-2020-11-17/Example_C.ps1
+++ Group-2020-11-17/Example_C.ps1
@@ -1,4 +1,4 @@
 python elas_C.py
 python crack_shear_C.py -t 2 -c const -p 200 | tee C12A-200.log
-python crack_shear_C.py -t 2 -c exp -p 200 | tee C12C-200.log
+python crack_shear_C.py -t 2 -c exp -p 200 | tee C12B-200.log
 echo done
